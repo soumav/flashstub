1c43e738c006876020d7158da53e1827a8cca178
abhishekaman3015 <abhishekaman3015@gmail.com>
2021-08-05 20:47:27 +0530

    rebasing with updated main branch

--- RegisterFS/src/main/java/com/demo/controller/RegisterController.java
+++ RegisterFS/src/main/java/com/demo/controller/RegisterController.java
@@ -3,17 +3,15 @@ package com.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.model.User;
@@ -25,7 +23,8 @@ public class RegisterController {
 	@Autowired
 	private RegisterService service;
 
-	@RequestMapping(value="/registerUser",method = RequestMethod.POST)
+
+	@PostMapping("/registerUser")
 	public ResponseEntity<Object> save(@RequestHeader(value = "corelation-id", required = false) String cid,
 			@RequestBody User user) {
 		HttpHeaders headers = new HttpHeaders();
