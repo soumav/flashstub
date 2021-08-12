0aef954398d7d5584217a626e3981b944c2e6024
Abhishek Kumar Aman <abhishekaman3015@gmail.com>
2021-08-12 10:19:30 +0530

    updated code (#4)

--- RegisterFS/src/main/java/com/demo/RegisterFsApplication.java
+++ RegisterFS/src/main/java/com/demo/RegisterFsApplication.java
@@ -3,7 +3,7 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.soumav.*", "com.demo.*" })
+@SpringBootApplication(scanBasePackages = {"com.commonfw.*", "com.demo.*" })
 public class RegisterFsApplication {
 
 	public static void main(String[] args) {
