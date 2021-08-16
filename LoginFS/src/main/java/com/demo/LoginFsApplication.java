8d8c225515786ce415c5945de8a43462d8b6a89d
Soumav <soumava91@gmail.com>
2021-08-17 02:30:27 +0530

    Feature (#7)
    * initial commit
    
    * updated pom.xml
    
    * implementing slf4j
    
    * updated code (#4)
    
    * adding eureka server
    
    * rearranging pom
    
    * eureka server+client integration + hystrix dependency version resolution
    
    * eureka server+client integration + hystrix dependency version resolution
    
    Co-authored-by: abhishekaman3015 <abhishekaman3015@gmail.com>

--- LoginFS/src/main/java/com/demo/LoginFsApplication.java
+++ LoginFS/src/main/java/com/demo/LoginFsApplication.java
@@ -3,10 +3,11 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication(scanBasePackages = {"com.commonfw.*", "com.demo.*" })
 @EnableDiscoveryClient
-//@EnableHystrix
+@EnableHystrix
 public class LoginFsApplication {
 
 	public static void main(String[] args) {
