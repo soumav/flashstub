aa21a6dfa4ddbbd7f867181fbdc1a039527449cb
Soumav <soumava91@gmail.com>
2021-08-17 02:21:17 +0530

    Feature (#6)
    * initial commit
    
    * updated pom.xml
    
    * implementing slf4j
    
    * updated code (#4)
    
    * adding eureka server
    
    * rearranging pom
    
    * eureka server+client integration + hystrix dependency version resolution
    
    Co-authored-by: abhishekaman3015 <abhishekaman3015@gmail.com>

--- LoginFS/src/main/java/com/demo/LoginFsApplication.java
+++ LoginFS/src/main/java/com/demo/LoginFsApplication.java
@@ -2,8 +2,11 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.commonfw.*", "com.demo.*" })
+@EnableDiscoveryClient
+//@EnableHystrix
 public class LoginFsApplication {
 
 	public static void main(String[] args) {
--- RegisterFS/src/main/java/com/demo/RegisterFsApplication.java
+++ RegisterFS/src/main/java/com/demo/RegisterFsApplication.java
@@ -2,8 +2,12 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication(scanBasePackages = {"com.commonfw.*", "com.demo.*" })
+@EnableDiscoveryClient
+@EnableHystrix
 public class RegisterFsApplication {
 
 	public static void main(String[] args) {
