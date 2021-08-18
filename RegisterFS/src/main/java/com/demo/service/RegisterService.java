8299fc426f4bdd3f28ba159aa0c62270c49ee093
Soumav <soumava91@gmail.com>
2021-08-18 16:28:24 +0530

    Feature (#11)
    * initial commit
    
    * updated pom.xml
    
    * implementing slf4j
    
    * updated code (#4)
    
    * adding eureka server
    
    * rearranging pom
    
    * eureka server+client integration + hystrix dependency version resolution
    
    * eureka server+client integration + hystrix dependency version resolution
    
    * removing un-used dependencies
    
    * modifying pom
    
    * commit for spring cloud gateway and reslience4j
    
    * working resilience4j
    
    Co-authored-by: abhishekaman3015 <abhishekaman3015@gmail.com>

--- RegisterFS/src/main/java/com/demo/service/RegisterService.java
+++ RegisterFS/src/main/java/com/demo/service/RegisterService.java
@@ -2,22 +2,35 @@ package com.demo.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.demo.model.User;
 import com.demo.repository.UserRepository;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 //@Slf4j
 public class RegisterService {
 
+	Logger LOGGER = LoggerFactory.getLogger(RegisterService.class);
+
 	@Autowired
 	private UserRepository repository;
 
-	public void registerUser(User user) {   
+	@CircuitBreaker(name = "registerservice", fallbackMethod = "fallbackRegisterService")
+	public String registerUser(User user) {
 		repository.save(user);
-		//log.info("Successfully Saved");
+		LOGGER.info("Successfully Saved");
+		return "User successfully registered";
+	}
+
+	public String fallbackRegisterService(User user, Throwable t) {
+		LOGGER.error("Exception occured while querying DB.", t);
+		return "Please try again in sometime!";
 	}
 
 	public List<User> getUsers() {
